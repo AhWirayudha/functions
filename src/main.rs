7b52e99139d76193522b34de2caa62a72d2e1adc
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-04-17 13:53:13 +0700

    parameters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
+    println!("{}", default_param(5, 6));
+    print_label_measurement('h', 42);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("Another function. {x}");
 }
+
+//default parameters on function
+fn default_param(x: i32, y: i32) -> i32 {
+   println!("{} + {} = {}", x, y, x + y);
+   x + y
+}
+//using char is different from string
+fn print_label_measurement(label: char, measurement: i32) {
+    println!("{label}: {measurement}");
+}
+
